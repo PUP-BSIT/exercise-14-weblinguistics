ebb050a52e91574329ca1f7368e795a6fe91f75d
Diana Rose Fidel <fideldianarose@gmail.com>
2023-11-13 23:19:55 +0800

    add update

--- fidel_dianarose/scripttt.js
+++ fidel_dianarose/scripttt.js
@@ -21,3 +21,30 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    function compareDates(a, b) {
+        const dateA = new Date(a.getAttribute('data-date'));
+        const dateB = new Date(b.getAttribute('data-date'));
+        return dateA - dateB;
+    }
+    function sortComments(order) {
+      const commentsContainer = document.getElementById('comments');
+      const comments = Array.from(commentsContainer.getElementsByTagName
+	  ('p'));
+
+        comments.sort(compareDates);
+
+        if (order === 'desc') {
+            comments.reverse();
+        }
+        comments.forEach(function (comment) {
+            commentsContainer.appendChild(comment);
+        });
+    }
+sortComments('asc');
+		const sortDropdown = document.getElementById('sort');
+		sortDropdown.addEventListener('change', function () {
+        const selectedOrder = sortDropdown.value;
+        sortComments(selectedOrder);
+    });
+});
