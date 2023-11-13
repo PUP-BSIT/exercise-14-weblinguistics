c4c70ac7665d7033dd8b0315f4e9d383facecffd
diana <fideldianarose@gmail.com>
2023-11-13 23:51:50 +0800

    add sorting function

--- fidel_dianarose/scripttt.js
+++ fidel_dianarose/scripttt.js
@@ -12,12 +12,39 @@ document.addEventListener('DOMContentLoaded', function () {
             newComment.className = 'comment_container';
             newComment.innerHTML = `<strong>Name: ${name}
 						</strong><br>${comment}<br><br>`;
-
+						
             commentsContainer.insertBefore
 			(newComment,commentsContainer.firstChild);
 
             document.getElementById('name').value = '';
             commentText.value = '';
         }
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    function compareDates(a, b) {
+        const dateA = new Date(a.getAttribute('data-date'));
+        const dateB = new Date(b.getAttribute('data-date'));
+        return dateA - dateB;
+    }
+    function sortComments(order) {
+      const commentsContainer = document.getElementById('comments');
+      const comments = Array.from(commentsContainer.getElementsByTagName
+	  ('p'));
+	  
+        comments.sort(compareDates);
+
+        if (order === 'desc') {
+            comments.reverse();
+        }
+        comments.forEach(function (comment) {
+            commentsContainer.appendChild(comment);
+        });
+    }
+sortComments('asc');
+		const sortDropdown = document.getElementById('sort');
+		sortDropdown.addEventListener('change', function () {
+        const selectedOrder = sortDropdown.value;
+        sortComments(selectedOrder);
     });
 });
